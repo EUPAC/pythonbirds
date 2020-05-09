326ed98859a60846308734b45c43ec7f00a87dac
eupac <eugenio@sisger.com.br>
2020-05-09 14:10:32 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self,nome=None,idade=None):
+    def __init__(self,*filhos, nome=None,idade=None):
         self.idade = idade
         self.nome = nome
+        self.filhos=list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        self_ = f'Olá {id(self)}'
+        return self_
 
 if __name__=='__main__':
-    p = Pessoa("Eugênio")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome='José Romualdo'
-    print(p.nome)
-    print(p.idade)
+    eugenio = Pessoa(nome="Eugênio")
+    luciano = Pessoa(eugenio,nome="Luciano")
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
